e0b75545876101d27f96e94ef7a90ac73a66f44a
Anil <ianilyurtsever@gmail.com>
2021-08-29 12:07:38 +0000

    userEntity impl

---
+++ src/main/java/MSGUserService/models/entities/UserEntity.java
@@ -0,0 +1,35 @@
+package MSGUserService.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_code")
+    private int userCode;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password_digest")
+    private String passwordDigest;
+
+}
