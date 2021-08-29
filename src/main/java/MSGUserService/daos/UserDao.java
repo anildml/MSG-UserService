eab373222e99a04ae310bbb545b4e467b590735a
Anil <ianilyurtsever@gmail.com>
2021-08-29 12:08:09 +0000

    userDao impl

---
+++ src/main/java/MSGUserService/daos/UserDao.java
@@ -0,0 +1,10 @@
+package MSGUserService.daos;
+
+import MSGUserService.models.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<UserEntity, Long> {
+
+}
