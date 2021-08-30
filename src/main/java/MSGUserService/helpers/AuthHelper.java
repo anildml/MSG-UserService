d20364cffd8ee5d5a2fbfa4d49bb86f4bcfd294a
Anil <ianilyurtsever@gmail.com>
2021-08-30 22:59:46 +0300

    authHelper impl
    jwtParser impl

--- src/main/java/MSGUserService/helpers/AuthHelper.java
+++ src/main/java/MSGUserService/helpers/AuthHelper.java
@@ -1,9 +1,13 @@
 package MSGUserService.helpers;
 
 import MSGUserService.models.dtos.UserDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 public interface AuthHelper {
+
+    String getUsernameFromToken(String token);
 
     String buildTokenForUser(UserDto userDto);
 
@@ -11,6 +15,18 @@ public interface AuthHelper {
 
 @Service
 class AuthHelperImpl implements AuthHelper {
+
+    private final String secret = "test";
+
+    @Override
+    public String getUsernameFromToken(String token) {
+        Claims claims = getAllClaimsFromToken(token);
+        return claims.getSubject();
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
 
     @Override
     public String buildTokenForUser(UserDto userDto) {
