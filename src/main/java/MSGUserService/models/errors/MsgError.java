7620e3b052daee5d0d189288ceac948fd0947fa1
Anil <ianilyurtsever@gmail.com>
2021-09-13 20:11:46 +0300

    error definitions added as comment to MsgError.java

--- src/main/java/MSGUserService/models/errors/MsgError.java
+++ src/main/java/MSGUserService/models/errors/MsgError.java
@@ -5,9 +5,19 @@ import lombok.Getter;
 @Getter
 public abstract class MsgError extends RuntimeException {
 
-    /*
-    MOVE AppError DEFINITIONS HERE
-    DEFINE ERROR CODES AND ERROR MESSAGES AND REASONS HERE
+    /**
+     * LOGIN ERRORS
+     *      0-0     Invalid Password
+     *      0-1     Invalid Username
+     *
+     *  SIGN UP ERRORS
+     *      1-0     Invalid Email
+     *      1-1     Invalid Password
+     *      1-2     Invalid Username
+     *
+     *  VALIDATION ERRORS
+     *      2-0     Invalid User Code
+     *      2-1     Token Not Valid
      */
 
     private final String errorCode;
