6b9b68020ed00a47b88b6caaa0a7fde1b1e8c067
Anil <ianilyurtsever@gmail.com>
2021-09-19 18:41:31 +0300

    userDao updates
    findByUserCode function impl
    function structures changed to implement @Query

--- src/main/java/MSGUserService/daos/UserDao.java
+++ src/main/java/MSGUserService/daos/UserDao.java
@@ -1,12 +1,17 @@
 package MSGUserService.daos;
 
 import MSGUserService.models.entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends CrudRepository<UserEntity, Long> {
 
+    @Query("SELECT u FROM UserEntity u WHERE u.username = ?1")
     UserEntity findUserEntityByUsername(String username);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.userCode = ?1")
+    UserEntity findUserEntityByUserCode(long userCode);
 
 }
