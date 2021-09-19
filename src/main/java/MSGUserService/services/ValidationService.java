a7768ee96615b7bccb993e64254dd2ad4cd88865
Anil <ianilyurtsever@gmail.com>
2021-09-19 18:45:00 +0300

    validation service updated
    funciton signature changed to return renewed token

--- src/main/java/MSGUserService/services/ValidationService.java
+++ src/main/java/MSGUserService/services/ValidationService.java
@@ -1,6 +1,9 @@
 package MSGUserService.services;
 
+import MSGUserService.daos.UserDao;
 import MSGUserService.helpers.AuthHelper;
+import MSGUserService.helpers.DtoMapper;
+import MSGUserService.models.dtos.UserDto;
 import MSGUserService.models.errors.validation.TokenNotValidError;
 import MSGUserService.models.errors.validation.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 public interface ValidationService {
 
-    Boolean validate(Long userCode, String token) throws ValidationError;
+    String validateAndCreateNewToken(Long userCode, String token) throws ValidationError;
 
 }
 
@@ -18,14 +21,21 @@ class ValidationServiceImpl implements ValidationService {
     @Autowired
     private AuthHelper authHelper;
 
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private DtoMapper dtoMapper;
+
 
     @Override
-    public Boolean validate(Long userCode, String token) throws ValidationError {
+    public String validateAndCreateNewToken(Long userCode, String token) throws ValidationError {
         Boolean isTokenValid = authHelper.isTokenValid(userCode, token);
         if (!isTokenValid) {
             throw new TokenNotValidError();
         }
-        return isTokenValid;
+        UserDto userDto = dtoMapper.convertToUserDto(userDao.findUserEntityByUserCode(userCode));
+        return authHelper.buildTokenForUser(userDto);
     }
 
 }
