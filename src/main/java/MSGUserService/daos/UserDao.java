9b5836d78ce9b826b9925deb6b480f17778d5456
Anil Yurtsever <admin>
2022-03-23 13:17:30 +0300

    feature
    repository extends

--- src/main/java/MSGUserService/daos/UserDao.java
+++ src/main/java/MSGUserService/daos/UserDao.java
@@ -1,13 +1,13 @@
 package MSGUserService.daos;
 
 import MSGUserService.models.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends CrudRepository<UserEntity, Long> {
+public interface UserDao extends JpaRepository<UserEntity, Long> {
 
     @Query("" +
             "SELECT u " +
