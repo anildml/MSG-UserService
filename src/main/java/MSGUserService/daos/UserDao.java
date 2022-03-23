451012fdb064428e5ca2ddefbff70d75feb59b35
Anil Yurtsever <admin>
2022-03-23 13:17:04 +0300

    feature
    repository functions updated

--- src/main/java/MSGUserService/daos/UserDao.java
+++ src/main/java/MSGUserService/daos/UserDao.java
@@ -3,15 +3,30 @@ package MSGUserService.daos;
 import MSGUserService.models.entities.UserEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends CrudRepository<UserEntity, Long> {
 
-    @Query("SELECT u FROM UserEntity u WHERE u.username = ?1")
-    UserEntity findUserEntityByUsername(String username);
+    @Query("" +
+            "SELECT u " +
+            "FROM UserEntity u " +
+            "WHERE (" +
+            "   u.username = :username" +
+            ")")
+    UserEntity findUserEntityByUsername(
+            @Param("username") String username
+    );
 
-    @Query("SELECT u FROM UserEntity u WHERE u.userCode = ?1")
-    UserEntity findUserEntityByUserCode(long userCode);
+    @Query("" +
+            "SELECT u " +
+            "FROM UserEntity u " +
+            "WHERE (" +
+            "   u.userCode = :userCode" +
+            ")")
+    UserEntity findUserEntityByUserCode(
+            @Param("userCode") long userCode
+    );
 
 }
